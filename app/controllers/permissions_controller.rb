1e734fb04a9e26cc5e6adab7eb0ae8b8a12bf466
Esteban Hurtado <webesteban@hotmail.com>
2025-05-28 04:01:08 -0500

    create permissions

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,4 +1,43 @@
 class PermissionsController < ApplicationController
   def index
+    @permissions = Permission.all
+  end
+  
+  def new
+    @permission = Permission.new
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+  
+  def edit
+    @permission = Permission.find(params[:id])
+    respond_to do |format|
+      format.turbo_stream
+    end
+  end
+  
+  def create
+    @permission = Permission.new(permission_params)
+    if @permission.save
+      redirect_to permissions_path, notice: "Modulo creada exitosamente"
+    else
+      render :new, status: :unprocessable_entity, formats: [:turbo_stream]
+    end
+  end
+  
+  def update
+    @permission = Permission.find(params[:id])
+    if @permission.update(permission_params)
+      redirect_to permissions_path, notice: "Modulo actualizada exitosamente"
+    else
+      render :edit, status: :unprocessable_entity, formats: [:turbo_stream]
+    end
+  end
+  
+  private
+  
+  def permission_params
+    params.require(:permission).permit(:name, :code, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get "permissions/index"
-  get "sections/index"
-  get "sections/new"
-  get "sections/edit"
-
   resources :sections
+  resources :permissions
+  resources :roles
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
